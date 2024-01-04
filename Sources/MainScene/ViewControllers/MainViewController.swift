77fcbdb2aff80f1341be2a293fa6fdb604388edf
jinsejin <j03298969@gmail.com>
2024-01-04 19:11:49 +0900

    [Feat] PomodoroTimePickerDelegate 프로토콜을 이용해 타이머 데이터 전달

--- Sources/MainScene/ViewControllers/MainViewController.swift
+++ Sources/MainScene/ViewControllers/MainViewController.swift
@@ -60,7 +60,6 @@ final class MainViewController: UIViewController,TimeSettingViewControllerDelega
             UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [id])
         }
     }
-
     func didSelectTime(time: Int) {
         maxTime = time * 60
     }
@@ -69,7 +68,6 @@ final class MainViewController: UIViewController,TimeSettingViewControllerDelega
         timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
             let minutes = (self.maxTime - self.currentTime) / 60
             let seconds = (self.maxTime - self.currentTime) % 60
-            
             self.timeLabel.text = String(format: "%02d:%02d", minutes, seconds)
             self.currentTime += 1
 
