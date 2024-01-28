8853bd02d0198a7d3762d5509be3c5bf09e0f3cd
haram22 <kimhr9889@handong.ac.kr>
2024-01-28 12:11:26 +0900

    [chore] git pull

--- Sources/DashBoardScene/Views/MockData.swift
+++ Sources/DashBoardScene/Views/MockData.swift
@@ -5,9 +5,7 @@
 //  Created by 김하람 on 2023/11/27.
 //  Copyright © 2023 io.hgu. All rights reserved.
 //
-
 import UIKit
-
 // FIXME: - 임시로 사용하는 PomodoroData입니다.
 struct PomodoroData {
     var breakTime: Int
@@ -22,10 +20,10 @@ struct PomodoroData {
         let defaultDate = Date()
         
         return [
-            PomodoroData(breakTime: 5, focusTime: 25, tagId: "공부", participateDate: dateFormatter.date(from: "2024-01-30") ?? defaultDate, success: true),
-            PomodoroData(breakTime: 5, focusTime: 30, tagId: "운동", participateDate: dateFormatter.date(from: "2024-01-28") ?? defaultDate, success: false),
-            PomodoroData(breakTime: 5, focusTime: 25, tagId: "스터디", participateDate: dateFormatter.date(from: "2024-01-28") ?? defaultDate, success: true),
-            PomodoroData(breakTime: 5, focusTime: 25, tagId: "스터디", participateDate: dateFormatter.date(from: "2024-01-08") ?? defaultDate, success: false),
+            PomodoroData(breakTime: 5, focusTime: 25, tagId: "공부", participateDate: dateFormatter.date(from: "2024-01-01") ?? defaultDate, success: true),
+            PomodoroData(breakTime: 5, focusTime: 30, tagId: "운동", participateDate: dateFormatter.date(from: "2024-01-04") ?? defaultDate, success: false),
+            PomodoroData(breakTime: 5, focusTime: 25, tagId: "운동", participateDate: dateFormatter.date(from: "2024-01-04") ?? defaultDate, success: true),
+            PomodoroData(breakTime: 5, focusTime: 25, tagId: "운동", participateDate: dateFormatter.date(from: "2024-01-08") ?? defaultDate, success: false),
             PomodoroData(breakTime: 5, focusTime: 25, tagId: "운동", participateDate: dateFormatter.date(from: "2024-01-12") ?? defaultDate, success: false),
             PomodoroData(breakTime: 5, focusTime: 25, tagId: "공부", participateDate: dateFormatter.date(from: "2024-01-09") ?? defaultDate, success: true),
             PomodoroData(breakTime: 5, focusTime: 25, tagId: "공부", participateDate: dateFormatter.date(from: "2024-01-02") ?? defaultDate, success: true),
